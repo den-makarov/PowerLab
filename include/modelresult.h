642097f0a4b0aa098f3c8ed32eda628c218419cc
Denis Makarov <denis.makarov@softeq.com>
2020-09-06 23:34:39 +0300

    provide methods to add data

--- include/modelresult.h
+++ include/modelresult.h
@@ -6,18 +6,24 @@
 #include <utility>
 #include <QObject>
 
+#define E_MODEL_RESULT_STRICT_CHECK
+
 class ModelResult : public QObject
 {
   Q_OBJECT
 public:
+  using DataPoints = std::vector<double>;
+  using DataNames = std::pair<std::string, std::string>;
   explicit ModelResult(size_t variables, size_t points, QObject *parent = nullptr);
 
   size_t getVariablesNumber() const;
   size_t getPointsNumber() const;
+  void addDataPoint(size_t var, double point);
+  void setDataNames(size_t var, DataNames& names);
 
 private:
-  std::vector<std::vector<double>> m_data;
-  std::vector<std::pair<std::string, std::string>> m_signals;
+  std::vector<DataPoints> m_data;
+  std::vector<DataNames> m_signals;
 };
 
 #endif // MODELRESULT_H
--- modelresult.cpp
+++ modelresult.cpp
@@ -3,9 +3,9 @@
 
 ModelResult::ModelResult(size_t variables, size_t points, QObject *parent) : QObject(parent)
 {
-  if(variables < 1) {
+  if(variables == 0) {
     E_WARNING(this) << "invalid size of variables"
-                     << variables;
+                    << variables;
 
     variables = 1;
   }
@@ -13,15 +13,15 @@ ModelResult::ModelResult(size_t variables, size_t points, QObject *parent) : QOb
   if(points <= 1) {
     E_WARNING(this) << "number of points too small"
                     << points;
-    points = 2;
+    points = 0;
   }
 
-  m_data = std::vector<std::vector<double>>(variables);
+  m_data = std::vector<DataPoints>(variables);
   for(auto& item : m_data) {
-    item = std::vector<double>(points, 0.0);
+    item.reserve(points);
   }
 
-  m_signals = std::vector<std::pair<std::string, std::string>>(variables);
+  m_signals = std::vector<DataNames>(variables);
   for(auto& item : m_signals) {
     item = std::make_pair<std::string, std::string>("NO_NAME", "NO_UNIT");
   }
@@ -32,5 +32,35 @@ size_t ModelResult::getVariablesNumber() const {
 }
 
 size_t ModelResult::getPointsNumber() const {
+  if(m_data.empty()) {
+    E_CRITICAL(this) << "Invalid container state";
+    return 0;
+  }
+
   return m_data[0].size();
+}
+
+void ModelResult::addDataPoint(size_t var, double point) {
+#ifdef E_MODEL_RESULT_STRICT_CHECK
+  if(m_data.empty()) {
+    E_CRITICAL(this) << "Invalid container";
+    return;
+  }
+#endif
+
+  if(var >= m_data[0].size()) {
+    E_CRITICAL(this) << "Invalid identifier";
+    return;
+  }
+
+  m_data[var].push_back(point);
+}
+
+void ModelResult::setDataNames(size_t var, DataNames& names) {
+  if(var >= m_signals.size()) {
+    E_CRITICAL(this) << "Invalid identifier";
+    return;
+  }
+
+  m_signals[var] = names;
 }
