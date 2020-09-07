10b7c666bf8febee205a07d4c7821219c4800255
Denis Makarov <denis.makarov@softeq.com>
2020-09-07 22:45:06 +0300

    Fix debug messages

--- modelresult.cpp
+++ modelresult.cpp
@@ -6,8 +6,8 @@ ModelResult::DataNames ModelResult::dummyNames = std::make_pair<std::string, std
 
 ModelResult::ModelResult(size_t variables, size_t points, QObject *parent) : QObject(parent)
 {
-  E_DEBUG(this) << "New storage for #"
-                << variables << "variables, #"
+  E_DEBUG(this) << "New storage for"
+                << variables << "variables,"
                 << points << "points";
 
   m_data = std::vector<DataPoints>(variables);
@@ -39,8 +39,8 @@ void ModelResult::addDataPoint(size_t var, double point) {
     return;
   }
 
-  E_DEBUG(this) << "New data point for variable id " << var
-                << ", point " << point;
+  E_DEBUG(this) << "New data point for variable id" << var
+                << ", point" << point;
   m_data[var].push_back(point);
 }
 
@@ -50,8 +50,8 @@ void ModelResult::addDataPoint(size_t var, const DataPoints& data) {
     return;
   }
 
-  E_DEBUG(this) << "New data point for variable id " << var
-                << ", points number " << data.size();
+  E_DEBUG(this) << "New data point for variable id" << var
+                << ", points number" << data.size();
 
   m_data[var].insert(m_data[var].end(), data.begin(), data.end());
 }
@@ -62,9 +62,9 @@ void ModelResult::setDataNames(size_t var, DataNames& names) {
     return;
   }
 
-  E_DEBUG(this) << "Update variable id " << var
-                << ", title: " << names.first.c_str()
-                << ", units: " << names.second.c_str();
+  E_DEBUG(this) << "Update variable id" << var
+                << ", title:" << names.first.c_str()
+                << ", units:" << names.second.c_str();
 
   m_signals[var] = names;
 }
