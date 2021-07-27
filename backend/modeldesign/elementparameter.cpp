e06a52d25e8267d40e2ef0c3d0e1f7e50e274f64
Denis Makarov <armature.current@gmail.com>
2021-07-27 21:41:30 +0300

    Provide implementation of parameters

--- backend/modeldesign/elementparameter.cpp
+++ backend/modeldesign/elementparameter.cpp
@@ -1,7 +1,108 @@
+#include <limits>
+#include <map>
+
 #include "elementparameter.h"
+#include "logger.h"
+
+namespace {
+const std::map<PowerLab::ModelDesign::ParameterType, std::string> UNITS_MAP = {
+  { PowerLab::ModelDesign::ParameterType::RESISTANCE, "Ohm" },
+  { PowerLab::ModelDesign::ParameterType::INDUCTANCE, "H" },
+  { PowerLab::ModelDesign::ParameterType::CAPACITANCE, "F" },
+  { PowerLab::ModelDesign::ParameterType::VOLTAGE, "V" },
+  { PowerLab::ModelDesign::ParameterType::CURRENT, "A" },
+  { PowerLab::ModelDesign::ParameterType::TEMPERATURE, "C" },
+  { PowerLab::ModelDesign::ParameterType::LENGTH, "m" },
+  { PowerLab::ModelDesign::ParameterType::WIDTH, "m" },
+  { PowerLab::ModelDesign::ParameterType::AREA, "m2" },
+  { PowerLab::ModelDesign::ParameterType::VOLUME, "m3" },
+  { PowerLab::ModelDesign::ParameterType::QUANTITY, "" },
+};
+}
 
 namespace PowerLab {
 namespace ModelDesign {
+
+ElementParameter::ElementParameter(const std::string& name)
+  : m_name(name)
+{
+}
+
+const std::string& ElementParameter::getName() const {
+  return m_name;
+}
+
+ParameterType ElementParameter::getType() const {
+  return ParameterType::QUANTITY;
+}
+
+const std::string& ElementParameter::getUnits() const {
+  return UNITS_MAP.at(ElementParameter::getType());
+}
+
+void ElementParameter::getValue(const double** pointer) const {
+  static double INVALID_VALUE = std::numeric_limits<double>::quiet_NaN();
+  *pointer = &INVALID_VALUE;
+}
+
+void ElementParameter::setValue(double) {
+
+}
+
+void ElementParameter::getValue(const int** pointer) const {
+  static int INVALID_VALUE = std::numeric_limits<int>::min();
+  *pointer = &INVALID_VALUE;
+}
+
+void ElementParameter::setValue(int) {
+
+}
+
+void ElementParameter::getValue(const std::string** pointer) const {
+  static std::string INVALID_VALUE = "INVALID_VALUE";
+  *pointer = &INVALID_VALUE;
+}
+
+void ElementParameter::setValue(const std::string&) {
+}
+
+Resistance::Resistance(const std::string& name, double value)
+  : ElementParameter(name)
+  , m_value(value)
+{
+}
+
+ParameterType Resistance::getType() const {
+  return ParameterType::RESISTANCE;
+}
+
+const std::string& Resistance::getUnits() const {
+  return UNITS_MAP.at(getType());
+}
+
+void Resistance::getValue(const double** pointer) const {
+  *pointer = &m_value;
+}
+
+void Resistance::setValue(double value) {
+  m_value = value;
+}
+
+void Resistance::getValue(const int** pointer) const {
+  ElementParameter::getValue(pointer);
+}
+
+void Resistance::setValue(int value) {
+  ElementParameter::setValue(value);
+}
+
+void Resistance::getValue(const std::string** pointer) const {
+  ElementParameter::getValue(pointer);
+}
+
+void Resistance::setValue(const std::string& value) {
+  ElementParameter::setValue(value);
+}
 
 } // namespace ModelDesign
 } // namespace PowerLab
--- backend/modeldesign/elementparameter.h
+++ backend/modeldesign/elementparameter.h
@@ -7,7 +7,85 @@
 namespace PowerLab {
 namespace ModelDesign {
 
-using ElementParameter = std::variant<int, double, std::string>;
+enum class ParameterType {
+  RESISTANCE,
+  INDUCTANCE,
+  CAPACITANCE,
+  VOLTAGE,
+  CURRENT,
+  TEMPERATURE,
+  LENGTH,
+  WIDTH,
+  AREA,
+  VOLUME,
+  QUANTITY
+};
+
+class ElementParameter {
+public:
+  explicit ElementParameter(const std::string& name);
+  virtual ~ElementParameter() = default;
+
+  const std::string& getName() const;
+  virtual ParameterType getType() const = 0;
+  virtual const std::string& getUnits() const = 0;
+
+  template<typename T>
+  const auto& getValue() const {
+    constexpr bool supportedType = std::is_same<T, std::string>::value
+                                || std::is_same<T, int>::value
+                                || std::is_same<T, double>::value;
+    static_assert(supportedType, "Parameter type isn't supported");
+
+    const T** pointer;
+    getValue(pointer);
+    return **pointer;
+  }
+
+  template<typename T>
+  void setValue(const T& value) {
+    constexpr bool supportedType = std::is_same<T, std::string>::value
+                                || std::is_same<T, int>::value
+                                || std::is_same<T, double>::value;
+    static_assert(supportedType, "Parameter type isn't supported");
+
+    setValue(value);
+  }
+
+protected:
+  virtual void getValue(const double** pointer) const = 0;
+  virtual void setValue(double value) = 0;
+
+  virtual void getValue(const int** pointer) const = 0;
+  virtual void setValue(int pointer) = 0;
+
+  virtual void getValue(const std::string** pointer) const = 0;
+  virtual void setValue(const std::string& pointer) = 0;
+
+private:
+  const std::string m_name;
+};
+
+class Resistance : public ElementParameter {
+public:
+  Resistance(const std::string& name, double value = 1.0);
+  virtual ~Resistance() override = default;
+
+  virtual ParameterType getType() const override;
+  virtual const std::string& getUnits() const override;
+
+private:
+  virtual void getValue(const double** pointer) const override;
+  virtual void setValue(double value) override;
+
+  virtual void getValue(const int** pointer) const override;
+  virtual void setValue(int value) override;
+
+  virtual void getValue(const std::string** pointer) const override;
+  virtual void setValue(const std::string& value) override;
+
+  double m_value;
+};
 
 } // namespace ModelDesign
 } // namespace PowerLab
